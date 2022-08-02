921034538329aea9d2ec433c8f1d20ad8359b897
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-08-02 09:22:55 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-flatpak remote-add tuxemonRepo tuxemonRepo --no-gpg-verify
+flatpak remote-delete tuxemonRepo
+flatpak remote-add tuxemonRepo tuxemonRepo --no-gpg-verify --if-not-exists
 flatpak install tuxemonRepo org.tuxemon.Tuxemon
